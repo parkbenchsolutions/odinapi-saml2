d0fc43648366d6e606d1ffd27c7b64e731fcd9f0
Scott Latsa <slatsa@parkbenchsolutions.com>
2021-06-23 10:37:27 -0400

    Reorganized the code

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Classes\Helpers;
-
 Route::middleware('App\Http\Middleware\Saml2')
 ->prefix('/api/v2/sso/saml2/')->group(function() {
     Route::prefix('odinsaml')->group(function() {
 
         $saml2_controller = 'App\Http\Controllers\Sso\Saml2Controller';
-        Helpers::debug('in saml2 routes');
 
         // dd($saml2_controller);
         Route::get('/logout', array(
